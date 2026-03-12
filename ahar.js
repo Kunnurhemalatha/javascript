a30a93daed600c2f8cf92d3fe7437de1e059fce0
Kunnurhemalatha <kammarahemalatha443@gmail.com>
2026-03-12 12:22:34 +0530

    Add files via upload

---
+++ ahar.js
@@ -0,0 +1,13 @@
+let adhar=false;
+let pan=true;
+if(adhar){
+    console.log("you can open the account in our bank");
+
+}else if(pan){
+
+     console.log("you can open the account in our bank");
+
+}else{
+     console.log("you can  not open the account in our bank");
+
+}
