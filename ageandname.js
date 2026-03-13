1e52796497d1cd6d109a3964ee1a4f06060b87f0
Kunnurhemalatha <kammarahemalatha443@gmail.com>
2026-03-13 11:06:22 +0530

    Add files via upload

---
+++ ageandname.js
@@ -0,0 +1,11 @@
+const person={
+    name:"hema";
+    age:19;
+    greet:function(){
+        return 'Hello,myname is $(this.Name)';
+    },};
+    console.log(person.name);
+    console.log(person.greet());
+
+
+    
